b6a3b87b2f47ceaabfc8c621badf20d59b76c6d3
Qasim Shareh <qshareh@uci.edu>
2018-08-27 23:28:18 -0400

    added basic structure for main file

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+
+from pygame.locals import *
+
+class Game(object)
+	
+	def __init__(self):
+		pass
+
+	def run(self):
+		pass
+
+if __name__ == "__main__":
+	Game().run()
